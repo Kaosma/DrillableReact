c6bef53caf87d1125725b956329d0d1a635bf130
Kaosma <erik-nu@hotmail.com>
2021-05-07 09:49:09 +0200

    Removed all old, unnecessary code.

--- tabScreens/ClipBoard/styles.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    backgroundColor: '#3a3535',
-    alignItems: 'center',
-  },
-});
